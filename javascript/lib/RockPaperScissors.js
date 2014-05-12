103913895552d82d587f3e790d3318963d5e0dda
shelleyhope <shelleyandkenny@mac.com>
2014-05-12 17:05:52 +0100

    first commit

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -12,6 +12,22 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
+Game.prototype.PAIRS = {
+												rock: {beats: "scissors"},
+												paper: {beats: "rock"},
+												scissors: {beats: "paper"}
+}
+
 Game.prototype.winner = function() {
-  return this.player1;
+	if(this._isSamePick()) return null;
+	if(this.PAIRS[this.player1.pick]["beats"] === this.player2.pick) {
+		return this.player1;
+	}
+	else {
+		return this.player2;
+	}
+};
+
+Game.prototype._isSamePick = function() {
+  return this.player1.pick === this.player2.pick;
 };
